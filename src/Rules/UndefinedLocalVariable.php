efd6489d6704272ebd95d670b34a14ba1615de6b
Nic Puddu <n.puddu@outlook.com>
2016-03-17 15:43:15 +0000

    Amended the UndefinedLocalVariable rule to make sure that it does allow the $matches variables in preg_match and preg_match_all functions to be undefined. Fixed indentation on all files.

--- src/Rules/UndefinedLocalVariable.php
+++ src/Rules/UndefinedLocalVariable.php
@@ -15,53 +15,51 @@ use PHPMD\Rule\AbstractLocalVariable;
 class UndefinedLocalVariable extends AbstractLocalVariable implements FunctionAware, MethodAware
 {
 
-	/**
+    /**
      * Collected ast nodes.
      *
      * @var \PHPMD\Node\ASTNode[]
      */
     private $nodes = [];
 
-	/**
+    /**
      * Collected closures.
      *
      * @var \PHPMD\Node\ASTNode[]
      */
-	private $closures = [];
+    private $closures = [];
 
-	/**
+    /**
      * Current Closure analysed.
      *
      * @var \PHPMD\Node\ASTNode[]
      */
-	private $currentClosure;
+    private $currentClosure;
 
-	/**
-	 * @param AbstractNode $node
-	 */
-	public function apply(AbstractNode $node)
+    /**
+     * @param AbstractNode $node
+     */
+    public function apply(AbstractNode $node)
     {
         if ($this->isAbstractMethod($node)) {
             return;
         }
 
         $this->nodes = [];
-		$this->closures = [];
-		$this->currentClosure = null;
+        $this->closures = [];
+        $this->currentClosure = null;
 
-		$this->getClosures($node);
+        $this->getClosures($node);
         $this->collectVariables($node);
-		$this->removeParameters($node);
+        $this->removeParameters($node);
         $this->removeDeclaredVariables();
+        $this->removePregMatchVariables();
         foreach ($this->nodes as $node) {
             $this->addViolation($node, array($node->getImage()));
         }
-
-		// method disabled because there is no way to get the use statement.
-		//$this->processClosuresVariables();
     }
 
-	/**
+    /**
      * Returns <b>true</b> when the given node is an abstract method.
      *
      * @param AbstractNode2 $node
@@ -75,19 +73,19 @@ class UndefinedLocalVariable extends AbstractLocalVariable implements FunctionAw
         return false;
     }
 
-	/**
-	 * Gets closures within the method or function.
-	 *
-	 * @param AbstractNode $node
-	 */
-	private function getClosures(AbstractNode $node)
-	{
-		$closures = $node->findChildrenOfType('Closure');
+    /**
+     * Gets closures within the method or function.
+     *
+     * @param AbstractNode $node
+     */
+    private function getClosures(AbstractNode $node)
+    {
+        $closures = $node->findChildrenOfType('Closure');
 
-		foreach ($closures as $closure) {
-			$this->closures[] = $closure;
-		}
-	}
+        foreach ($closures as $closure) {
+            $this->closures[] = $closure;
+        }
+    }
 
     /**
      * This method extracts all local variables for the given function or method node.
@@ -96,34 +94,34 @@ class UndefinedLocalVariable extends AbstractLocalVariable implements FunctionAw
      */
     private function collectVariables(AbstractNode $node)
     {
-		$variables = $node->findChildrenOfType('Variable');
+        $variables = $node->findChildrenOfType('Variable');
         foreach ($variables as $variable) {
-			if ($this->isLocal($variable) && $this->isNotInClosure($variable) && !isset($this->nodes[$variable->getImage()])) {
-				$this->nodes[$variable->getImage()] = $variable;
-			}
+            if ($this->isLocal($variable) && $this->isNotInClosure($variable) && !isset($this->nodes[$variable->getImage()])) {
+                $this->nodes[$variable->getImage()] = $variable;
+            }
 
         }
     }
 
-	/**
-	 * Checks whether or not the current variable is inside a closure.
-	 *
-	 * @param AbstractNode $node
-	 * @return boolean
-	 */
-	private function isNotInClosure(AbstractNode $node)
-	{
-		$variableLine = $node->getBeginLine();
-		foreach ($this->closures as $closure) {
-			if ($closure !== $this->currentClosure && $variableLine > $closure->getBeginLine() && $variableLine < $closure->getEndLine()) {
-				return false;
-			}
-		}
+    /**
+     * Checks whether or not the current variable is inside a closure.
+     *
+     * @param AbstractNode $node
+     * @return boolean
+     */
+    private function isNotInClosure(AbstractNode $node)
+    {
+        $variableLine = $node->getBeginLine();
+        foreach ($this->closures as $closure) {
+            if ($closure !== $this->currentClosure && $variableLine > $closure->getBeginLine() && $variableLine < $closure->getEndLine()) {
+                return false;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	/**
+    /**
      * This method removes from the stored list of local variables the ones that
      * are also found in the formal parameters of the given method or/and
      * function node.
@@ -141,60 +139,60 @@ class UndefinedLocalVariable extends AbstractLocalVariable implements FunctionAw
         }
     }
 
-	/**
+    /**
      * This method removes from the stored list of local variables the ones that have been declared.
      */
     private function removeDeclaredVariables()
     {
         foreach ($this->nodes as $variable) {
-			if (
-				$this->isChildOf($variable, 'AssignmentExpression') ||
-				$this->isChildOf($variable, 'ForeachStatement') ||
-				$this->isChildOf($variable, 'CatchStatement') ||
-				$this->isChildOf($variable, 'ListExpression') ||
-				(
-					$this->isChildOf($variable, 'UnaryExpression') &&
-					$this->isChildOf($variable->getParent(), 'ForeachStatement')
-				))  {
-				unset($this->nodes[$variable->getImage()]);
-			}
+            if (
+                $this->isChildOf($variable, 'AssignmentExpression') ||
+                $this->isChildOf($variable, 'ForeachStatement') ||
+                $this->isChildOf($variable, 'CatchStatement') ||
+                $this->isChildOf($variable, 'ListExpression') ||
+                (
+                    $this->isChildOf($variable, 'UnaryExpression') &&
+                    $this->isChildOf($variable->getParent(), 'ForeachStatement')
+                ))  {
+                unset($this->nodes[$variable->getImage()]);
+            }
 
         }
     }
 
-	/**
-	 * Checks whether or not a node is a children of a specific type.
-	 *
-	 * @param AbstractNode $node
-	 * @param string $type
-	 * @return boolean
-	 */
-	private function isChildOf(AbstractNode $node, $type)
-	{
-		$parent = $node->getParent();
+    /**
+     * Remove from the stored list of local variables the third argument of preg_match
+     * and preg_match_all functions.
+     */
+    private function removePregMatchVariables()
+    {
+        foreach ($this->nodes as $variable) {
+            $parent = $variable->getParent()->getParent();
+            if ($parent->isInstanceOf('FunctionPostfix') && in_array($parent->getImage(), ['preg_match', 'preg_match_all'])) {
+                $children = $parent->findChildrenOfType('Variable');
+                $numberOfChildren = count($children);
+                $lastParameter = array_pop($children);
+                if ($numberOfChildren === 3 && $lastParameter == $variable)
+                {
+                    unset($this->nodes[$variable->getImage()]);
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks whether or not a node is a children of a specific type.
+     *
+     * @param AbstractNode $node
+     * @param string $type
+     * @return boolean
+     */
+    private function isChildOf(AbstractNode $node, $type)
+    {
+        $parent = $node->getParent();
         if ($parent->isInstanceOf($type)) {
             return true;
         }
         return false;
-	}
-
-	/**
-	 * Process every closure and look for unused variables inside them.
-	 * This method is currently unused because there is no way to get variables
-	 * passed to the closure with the use keyword.
-	 */
-	private function processClosuresVariables()
-	{
-		foreach ($this->closures as $node) {
-			$this->currentClosure = $node;
-			$this->nodes = [];
-			$this->collectVariables($node);
-			$this->removeParameters($node);
-			$this->removeDeclaredVariables();
-			$this->removeVariablesPreviouslyDeclared();
-			foreach ($this->nodes as $node) {
-				$this->addViolation($node, array($node->getImage()));
-			}
-		}
-	}
+    }
 }
--- src/Rules/UnreachableCode.php
+++ src/Rules/UnreachableCode.php
@@ -14,17 +14,17 @@ use PHPMD\Rule\MethodAware;
 class UnreachableCode extends \PHPMD\AbstractRule implements FunctionAware, MethodAware
 {
 
-	/**
+    /**
      * Collected ast nodes.
      *
      * @var \PHPMD\Node\ASTNode[]
      */
     private $nodes = [];
 
-	/**
-	 * @param AbstractNode $node
-	 */
-	public function apply(AbstractNode $node)
+    /**
+     * @param AbstractNode $node
+     */
+    public function apply(AbstractNode $node)
     {
         if ($this->isAbstractMethod($node)) {
             return;
@@ -33,13 +33,13 @@ class UnreachableCode extends \PHPMD\AbstractRule implements FunctionAware, Meth
         $this->nodes = [];
 
         $this->collectStatements($node, 'ReturnStatement');
-		$this->collectStatements($node, 'ThrowStatement');
+        $this->collectStatements($node, 'ThrowStatement');
         foreach ($this->nodes as $node) {
             $this->addViolation($node, array($node->getImage()));
         }
     }
 
-	/**
+    /**
      * Returns <b>true</b> when the given node is an abstract method.
      *
      * @param AbstractNode2 $node
@@ -57,196 +57,196 @@ class UnreachableCode extends \PHPMD\AbstractRule implements FunctionAware, Meth
      * This method extracts all statements of a given type for the given function or method node.
      *
      * @param AbstractNode $node
-	 * @param string       $statementType
+     * @param string       $statementType
      */
     private function collectStatements(AbstractNode $node, $statementType)
     {
-		$statements = $node->findChildrenOfType($statementType);
+        $statements = $node->findChildrenOfType($statementType);
         foreach ($statements as $statement) {
-			if (
-				!$this->isAtEndOfMainScope($statement, $node) &&
-				!$this->isAtEndOfIfStatement($statement) &&
-				!$this->isAtEndOfSwitchStatement($statement) &&
-				!$this->isAtEndOfTry($statement) &&
-				!$this->isAtEndOfCatch($statement) &&
-				!$this->isAtEndOfFinally($statement) &&
-				!$this->isAtEndOfClosure($statement)
-				) {
-				$this->nodes[] = $statement;
-			}
-		}
+            if (
+                !$this->isAtEndOfMainScope($statement, $node) &&
+                !$this->isAtEndOfIfStatement($statement) &&
+                !$this->isAtEndOfSwitchStatement($statement) &&
+                !$this->isAtEndOfTry($statement) &&
+                !$this->isAtEndOfCatch($statement) &&
+                !$this->isAtEndOfFinally($statement) &&
+                !$this->isAtEndOfClosure($statement)
+                ) {
+                $this->nodes[] = $statement;
+            }
+        }
     }
 
-	/**
-	 * @param AbstractNode $node
-	 * @return boolean
-	 */
-	private function isAtEndOfMainScope(AbstractNode $node, AbstractNode $method)
-	{
-		if ($this->isChildOf($node, 'Scope') && $this->isFinalStatement($method, $node)) {
-			return true;
-		}
+    /**
+     * @param AbstractNode $node
+     * @return boolean
+     */
+    private function isAtEndOfMainScope(AbstractNode $node, AbstractNode $method)
+    {
+        if ($this->isChildOf($node, 'Scope') && $this->isFinalStatement($method, $node)) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * @param AbstractNode $node
-	 * @return boolean
-	 */
-	private function isAtEndOfIfStatement(AbstractNode $node)
-	{
-		if (
-			$this->isChildOf($node, 'ScopeStatement') &&
-			(
-				$this->isChildOf($node->getParent(), 'IfStatement') ||
-				$this->isChildOf($node->getParent(), 'ElseIfStatement') ||
-				$this->isChildOf($node->getParent(), 'ElseStatement')
-			) &&
-			$this->isFinalStatement($node->getParent(), $node)
-			) {
-			return true;
-		}
+    /**
+     * @param AbstractNode $node
+     * @return boolean
+     */
+    private function isAtEndOfIfStatement(AbstractNode $node)
+    {
+        if (
+            $this->isChildOf($node, 'ScopeStatement') &&
+            (
+                $this->isChildOf($node->getParent(), 'IfStatement') ||
+                $this->isChildOf($node->getParent(), 'ElseIfStatement') ||
+                $this->isChildOf($node->getParent(), 'ElseStatement')
+            ) &&
+            $this->isFinalStatement($node->getParent(), $node)
+            ) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * @param AbstractNode $node
-	 * @return boolean
-	 */
-	private function isAtEndOfSwitchStatement(AbstractNode $node)
-	{
-		if (
-			$this->isChildOf($node, 'SwitchLabel') &&
-			$this->isChildOf($node->getParent(), 'SwitchStatement') &&
-			$this->isFinalStatement($node->getParent(), $node, 0)
-			) {
-			return true;
-		}
+    /**
+     * @param AbstractNode $node
+     * @return boolean
+     */
+    private function isAtEndOfSwitchStatement(AbstractNode $node)
+    {
+        if (
+            $this->isChildOf($node, 'SwitchLabel') &&
+            $this->isChildOf($node->getParent(), 'SwitchStatement') &&
+            $this->isFinalStatement($node->getParent(), $node, 0)
+            ) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * @param AbstractNode $node
-	 * @return boolean
-	 */
-	private function isAtEndOfTry(AbstractNode $node)
-	{
-		if (
-			$this->isChildOf($node, 'ScopeStatement') &&
-			$this->isChildOf($node->getParent(), 'TryStatement') &&
-			$this->isFinalStatement($node->getParent(), $node) &&
-			$this->thereIsNoFinallyStatement($node) &&
-			$this->thereIsNoCodeBeyondTryCatch($node->getParent()->getParent())
-			) {
-			return true;
-		}
+    /**
+     * @param AbstractNode $node
+     * @return boolean
+     */
+    private function isAtEndOfTry(AbstractNode $node)
+    {
+        if (
+            $this->isChildOf($node, 'ScopeStatement') &&
+            $this->isChildOf($node->getParent(), 'TryStatement') &&
+            $this->isFinalStatement($node->getParent(), $node) &&
+            $this->thereIsNoFinallyStatement($node) &&
+            $this->thereIsNoCodeBeyondTryCatch($node->getParent()->getParent())
+            ) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * @param AbstractNode $node
-	 * @return boolean
-	 */
-	private function isAtEndOfCatch(AbstractNode $node)
-	{
-		if (
-			$this->isChildOf($node, 'ScopeStatement') &&
-			$this->isChildOf($node->getParent(), 'CatchStatement') &&
-			$this->isFinalStatement($node->getParent(), $node)
-			) {
-			return true;
-		}
+    /**
+     * @param AbstractNode $node
+     * @return boolean
+     */
+    private function isAtEndOfCatch(AbstractNode $node)
+    {
+        if (
+            $this->isChildOf($node, 'ScopeStatement') &&
+            $this->isChildOf($node->getParent(), 'CatchStatement') &&
+            $this->isFinalStatement($node->getParent(), $node)
+            ) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * @param AbstractNode $node
-	 * @return boolean
-	 */
-	private function isAtEndOfFinally(AbstractNode $node)
-	{
-		if (
-			$this->isChildOf($node, 'ScopeStatement') &&
-			$this->isChildOf($node->getParent(), 'FinallyStatement') &&
-			$this->isFinalStatement($node->getParent(), $node) &&
-			$this->thereIsNoCodeBeyondTryCatch($node->getParent()->getParent()->getParent())
-			) {
-			return true;
-		}
+    /**
+     * @param AbstractNode $node
+     * @return boolean
+     */
+    private function isAtEndOfFinally(AbstractNode $node)
+    {
+        if (
+            $this->isChildOf($node, 'ScopeStatement') &&
+            $this->isChildOf($node->getParent(), 'FinallyStatement') &&
+            $this->isFinalStatement($node->getParent(), $node) &&
+            $this->thereIsNoCodeBeyondTryCatch($node->getParent()->getParent()->getParent())
+            ) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * @param AbstractNode $node
-	 * @return type
-	 */
-	private function thereIsNoFinallyStatement(AbstractNode $node)
-	{
-		return $node->getParent()->getParent()->getFirstChildOfType('FinallyStatement') ? false : true;
-	}
+    /**
+     * @param AbstractNode $node
+     * @return type
+     */
+    private function thereIsNoFinallyStatement(AbstractNode $node)
+    {
+        return $node->getParent()->getParent()->getFirstChildOfType('FinallyStatement') ? false : true;
+    }
 
-	/**
-	 *
-	 * @param AbstractNode $tryNode
-	 * @return type
-	 */
-	private function thereIsNoCodeBeyondTryCatch(AbstractNode $tryNode)
-	{
-		return $this->isFinalStatement($tryNode->getParent(), $tryNode);
-	}
+    /**
+     *
+     * @param AbstractNode $tryNode
+     * @return type
+     */
+    private function thereIsNoCodeBeyondTryCatch(AbstractNode $tryNode)
+    {
+        return $this->isFinalStatement($tryNode->getParent(), $tryNode);
+    }
 
-	/**
-	 * @param AbstractNode $node
-	 * @return boolean
-	 */
-	private function isAtEndOfClosure(AbstractNode $node)
-	{
-		if (
-			$this->isChildOf($node, 'Scope') &&
-			$node->getParent()->getParent() !== null &&
-			$this->isChildOf($node->getParent(), 'Closure') &&
-			$this->isFinalStatement($node->getParent(), $node)
-			) {
-			return true;
-		}
+    /**
+     * @param AbstractNode $node
+     * @return boolean
+     */
+    private function isAtEndOfClosure(AbstractNode $node)
+    {
+        if (
+            $this->isChildOf($node, 'Scope') &&
+            $node->getParent()->getParent() !== null &&
+            $this->isChildOf($node->getParent(), 'Closure') &&
+            $this->isFinalStatement($node->getParent(), $node)
+            ) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 
-	/**
-	 * Checks whether or not a node is a children of a specific type.
-	 *
-	 * @param AbstractNode $node
-	 * @param string $type
-	 * @return boolean
-	 */
-	private function isChildOf(AbstractNode $node, $type)
-	{
-		$parent = $node->getParent();
+    /**
+     * Checks whether or not a node is a children of a specific type.
+     *
+     * @param AbstractNode $node
+     * @param string $type
+     * @return boolean
+     */
+    private function isChildOf(AbstractNode $node, $type)
+    {
+        $parent = $node->getParent();
         if ($parent->isInstanceOf($type)) {
             return true;
         }
         return false;
-	}
+    }
 
-	/**
-	 * @param AbstractNode $scope
-	 * @param AbstractNode $node
-	 * @param integer      $adjustment number of lines to add to the final line of the node being checked.
-	 * @return boolean
-	 */
-	private function isFinalStatement(AbstractNode $scope, AbstractNode $node, $adjustment = 1)
-	{
-		if ($node->getEndLine() + $adjustment === $scope->getEndLine()) {
-			return true;
-		}
+    /**
+     * @param AbstractNode $scope
+     * @param AbstractNode $node
+     * @param integer      $adjustment number of lines to add to the final line of the node being checked.
+     * @return boolean
+     */
+    private function isFinalStatement(AbstractNode $scope, AbstractNode $node, $adjustment = 1)
+    {
+        if ($node->getEndLine() + $adjustment === $scope->getEndLine()) {
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
 }
